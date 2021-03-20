536aa37796ff3bf6e82bf1b19f9558c13b83377f
yogesh choudhary <15choudharyyogesh@gmail.com>
2021-03-20 19:31:53 +0530

    sdet-11 first commit

---
+++ src/main/java/cmd1/gitcmd1.java
@@ -0,0 +1,15 @@
+package cmd1;
+
+public class gitcmd1 {
+	
+	
+	
+	public static void main(String[] args) {
+		
+		System.out.println("welcome to india");
+		
+		
+		
+	}
+
+}
---
+++ src/main/java/cmd1/gitcmd2.java
@@ -0,0 +1,12 @@
+package cmd1;
+
+public class gitcmd2 {
+	
+	
+	public static void main(String[] args) {
+		
+		
+System.out.println("class 2 created");
+	}
+
+}
